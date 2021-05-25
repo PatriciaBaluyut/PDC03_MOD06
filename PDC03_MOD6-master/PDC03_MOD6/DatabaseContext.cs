13e0063fd3865246850387ecbe2b70668e2a0f58
Patricia Baluyut <baluyut.patriciamarie@auf.edu.ph>
2021-05-26 01:18:13 +0800

    Add project files.

---
+++ PDC03_MOD6-master/PDC03_MOD6/DatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using PDC03_MOD6.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+ 
+namespace PDC03_MOD6
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<EmployeeModel> Employees { get; set; }
+        public DatabaseContext()
+        {
+            this.Database.EnsureCreated();
+        }
+        // overrides the OnConfigure Method 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "Employee.db");
+            optionsBuilder.UseSqlite($"Filename={dbPath}");
+        }
+    }
+}
